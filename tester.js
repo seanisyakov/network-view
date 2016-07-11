1d084278a0c34662698683a58afc30ffb6754c06
zhaoliangm <zhaoliang.ma@duke.edu>
2016-07-10 21:08:39 -0400

    Add files via upload

---
+++ tester.js
@@ -0,0 +1,75 @@
+var b  = {
+    "cameras":[
+    {"source": "Nikon D3300", "somethingThan" : "better than",
+	"target": "Canon EOS 70D", "dimension": "image quality", 
+	"text": "Nikon D3300 is rated 30% higher in image quality compared to the Canon EOS 70D"},
+	    
+	{"source": "Nikon D3300", "somethingThan" : "better than",
+	"target": "Canon EOS 70D", "dimension": "sensor size", 
+	"text": "The Nikon D3300 has a slightly larger sensor than the Canon EOS 70D"},
+	
+	{"source": "Canon EOS 70D", "somethingThan" : "better than",
+	"target": "Nikon D3300", "dimension": "autofocus", "text": "Canon EOS 70D is a smooth fast auto-focusing camera that is silent, however if you set it to auto mode and go take pictures they don't look as good as the Nikon D3300 on auto mode."},
+	    
+	{"source": "Nikon D3300", "somethingThan" : "better than",
+	    "target": "Canon EOS 70D", "dimension": "sensor size", 
+	    "text": "The Nikon D3300 has a slightly larger sensor than the Canon EOS 70D"},
+	
+	{"source": "Nikon D3300", "somethingThan" : "better than",
+	    "target": "Sony HX-300", "dimension": "low-light performance", 
+	    "text": "I've tried the HX-300 and it has nowhere near the low light performance of the Nikon D3300"},
+	
+	{"source": "Nikon D3300", "somethingThan" : "better than",
+	    "target": "Canon EOS 70D", "dimension": "convenience", 
+	    "text": "The Canon EOS 70D may be able to match the Nikon if you manually tweak things"},
+
+	{"source": "Canon EOS Rebel SL1 ", "somethingThan" : "better than",
+    "target": "Canon EOS Rebel T5i", "dimension": "size", 
+    "text": "Choose the SL1 for the most petite size"},
+
+     {"source": "Canon EOS 60D", "somethingThan" : "better than",
+    "target": "Canon EOS Rebel T5i", "dimension": "efficiency", 
+    "text": "the 60D for a quicker interface and a deeper buffer for raw files"},
+
+    {"source": "Canon EOS 7D", "somethingThan" : "better than",
+    "target": "Canon EOS Rebel T5i", "dimension": "efficiency", 
+    "text": "7D for even better motion-tracking"},
+
+    {"source": "Canon EOS Rebel T4i", "somethingThan" : "better than",
+    "target": "Canon EOS Rebel T5i", "dimension": "price", 
+    "text": "The T4i alone or with the 18-135 STM is equally compelling if it costs less"},
+
+    {"source": "Canon EOS Rebel SL1 ", "somethingThan" : "better than",
+    "target": "Canon EOS Rebel T5i", "dimension": "weight", 
+    "text": "but if you're really sweating the ounces, a mirrorless system or the SL1 is a better choice"},
+
+     {"source": "Nikon D5300", "somethingThan" : "better than",
+    "target": "Nikon D90", "dimension": "weight", "text": "It's a little smaller and lighter than my D90 and fits well in hand"},
+    
+    {"source": "Nikon D5300", "somethingThan" : "better than",
+    "target": "Nikon D7000", "dimension": "weight", "text": "tried a D7000 and found it to heavy and bulky for me the D5300 seems just right"},
+    
+    {"source": "Nikon D5300", "somethingThan" : "worse than",
+    "target": "Nikon D3300", "dimension": "weight", "text": "Possibly the d3300 would be a smarter choice due to being much cheaper and a little lighter"},
+    
+    {"source": "Nikon D5300", "somethingThan" : "better than",
+    "target": "Nikon D80", "dimension": "overall", "text": "My previous camera was a D80, so this was a significant upgrade and well worth it"},
+
+    {"source": "Canon EOS 70D", "somethingThan" : "the same as",
+    "target": "Canon EOS 60D", "dimension": "low-light performance", "text": "unfortunately they look almost identical"},
+    
+    {"source": "Canon EOS 70D", "somethingThan" : "worse than",
+    "target": "Canon EOS 6D", "dimension": "low-light performance", "text": "you will need to step into a full frame like the 6d or the 5D mark ii & iii to get a substantial difference in quality above 1600 ISO"},
+    
+    {"source": "Canon EOS 70D", "somethingThan" : "worse than",
+    "target": "Canon EOS 7D", "dimension": "overall", "text": " If you need a camera for only still photography you may be better off buying a 7D or full frame camera for the money "},
+    
+    {"source": "Canon EOS 70D", "somethingThan" : "the same as",
+    "target": "Canon EOS 60D", "dimension": "size", "text": "No surprise: it feels exactly like a 60D"},
+    
+    {"source": "Canon EOS 70D", "somethingThan" : "worse than",
+    "target": "Canon EOS 6D", "dimension": "autofocus", "text": "The 6D is two solid stops ahead if low-light is your highest priority."}
+
+    ]
+
+}
